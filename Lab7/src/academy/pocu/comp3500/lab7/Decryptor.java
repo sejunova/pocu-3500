6b6d7c6af7d31accae1038b0f203e17023ef7ab2
sejun <sejun@quryon.com>
2021-06-28 18:33:44 +0900

    [Lab7] 길이, 해시 둘 다 비교

--- Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
+++ Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
@@ -5,22 +5,25 @@ import java.util.HashMap;
 
 public class Decryptor {
     private Trie trie = new Trie();
-    private HashMap<Integer, Integer> maxWordLengthsInfo = new HashMap<>();
+    private HashMap<Integer, HashMap<Integer, Integer>> maxWordLengthsInfo = new HashMap<>();
 
     public Decryptor(final String[] codeWords) {
         for (String codeWord : codeWords) {
             trie.insert(codeWord);
-            maxWordLengthsInfo.merge(getHash(codeWord), 1, Integer::sum);
+            final int wordLength = codeWord.length();
+            maxWordLengthsInfo.putIfAbsent(wordLength, new HashMap<>());
+            maxWordLengthsInfo.get(wordLength).merge(getHash(codeWord), 1, Integer::sum);
         }
     }
 
     public String[] findCandidates(final String word) {
-        int hash = getHash(word);
-        if (!maxWordLengthsInfo.containsKey(hash)) {
+        final int hash = getHash(word);
+        final int wordLength = word.length();
+        if (!maxWordLengthsInfo.containsKey(wordLength) || !(maxWordLengthsInfo.get(wordLength).containsKey(hash))) {
             return new String[0];
         }
 
-        int maxCount = maxWordLengthsInfo.get(hash);
+        int maxCount = maxWordLengthsInfo.get(wordLength).get(hash);
         ArrayList<String> candidates = trie.search(word, maxCount);
         String[] result = new String[candidates.size()];
         for (int i = 0; i < result.length; i++) {
