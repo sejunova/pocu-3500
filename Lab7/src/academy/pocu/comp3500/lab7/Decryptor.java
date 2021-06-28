c662a2a20784e3fca6974efe9d5709b9f4c26cb3
sejun <sejun@quryon.com>
2021-06-28 15:13:28 +0900

    [Lab7] early exit 조건 추가

--- Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
+++ Lab7/src/academy/pocu/comp3500/lab7/Decryptor.java
@@ -1,18 +1,26 @@
 package academy.pocu.comp3500.lab7;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Decryptor {
     private Trie trie = new Trie();
+    private HashMap<Integer, Integer> maxWordLengthsInfo = new HashMap<>();
 
     public Decryptor(final String[] codeWords) {
         for (String codeWord : codeWords) {
             trie.insert(codeWord);
+            maxWordLengthsInfo.merge(codeWord.length(), 1, Integer::sum);
         }
     }
 
     public String[] findCandidates(final String word) {
-        ArrayList<String> candidates = trie.search(word);
+        if (!maxWordLengthsInfo.containsKey(word.length())) {
+            return new String[0];
+        }
+
+        int maxCount = maxWordLengthsInfo.get(word.length());
+        ArrayList<String> candidates = trie.search(word, maxCount);
         String[] result = new String[candidates.size()];
         for (int i = 0; i < result.length; i++) {
             result[i] = candidates.get(i);
--- Lab7/src/academy/pocu/comp3500/lab7/Trie.java
+++ Lab7/src/academy/pocu/comp3500/lab7/Trie.java
@@ -15,21 +15,25 @@ public class Trie {
         node.isWord = true;
     }
 
-    public ArrayList<String> search(String word) {
+    public ArrayList<String> search(String word, int maxCount) {
         ArrayList<String> ret = new ArrayList<>();
         HashMap<Character, Integer> counter = new HashMap<>();
         for (char c : word.toCharArray()) {
             counter.merge(c, 1, Integer::sum);
         }
-        searchRecursive(ret, new StringBuilder(), counter, word.length(), root);
+        searchRecursive(ret, new StringBuilder(word.length()), counter, word.length(), root, maxCount);
         return ret;
     }
 
-    private void searchRecursive(ArrayList<String> ret, StringBuilder chars, HashMap<Character, Integer> counter, int inputLength, TrieNode node) {
+    private void searchRecursive(ArrayList<String> ret, StringBuilder chars, HashMap<Character, Integer> counter, int inputLength, TrieNode node, int maxCount) {
         if (chars.length() == inputLength) {
             if (node.isWord) {
                 ret.add(chars.toString());
             }
+            return;
+        }
+
+        if (maxCount == ret.size()) {
             return;
         }
 
@@ -40,7 +44,7 @@ public class Trie {
                 chars.append(c);
                 counter.put(c, count - 1);
 
-                searchRecursive(ret, chars, counter, inputLength, node.children.get(c));
+                searchRecursive(ret, chars, counter, inputLength, node.children.get(c), maxCount);
 
                 chars.setLength(chars.length() - 1);
                 counter.put(c, count);
