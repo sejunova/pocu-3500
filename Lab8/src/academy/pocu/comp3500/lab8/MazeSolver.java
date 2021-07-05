59a1054abf2cd750161688b81ad39bae765c7782
sejun <sejun@quryon.com>
2021-07-05 11:23:58 +0900

    [Lab8] Point 클래스 원복

--- Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
+++ Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
@@ -7,6 +7,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public final class MazeSolver {
+    private static final int[] dx = new int[]{1, 0, -1, 0};
+    private static final int[] dy = new int[]{0, 1, 0, -1};
+
     public static List<Point> findPath(final char[][] maze, final Point start) {
         if (maze[start.getY()][start.getX()] == 'E') {
             List<Point> result = new ArrayList<>();
@@ -22,7 +25,7 @@ public final class MazeSolver {
         outer:
         while (!queue.isEmpty()) {
             Node cur = queue.removeFirst();
-            List<Point> nextAvailablePoints = cur.point.getNextAvailablePoints(maze);
+            List<Point> nextAvailablePoints = getNextAvailablePoints(maze, cur.point);
             for (Point nextPoint : nextAvailablePoints) {
                 if (maze[nextPoint.getY()][nextPoint.getX()] == 'E') {
                     exitPoint = cur.addChild(nextPoint);
@@ -38,6 +41,22 @@ public final class MazeSolver {
             return new ArrayList<>();
         }
         return exitPoint.getPaths();
+    }
+
+    private static List<Point> getNextAvailablePoints(final char[][] maze, Point point) {
+        List<Point> nextAvailablePoints = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            int nx = point.getX() + dx[i];
+            int ny = point.getY() + dy[i];
+            if (nx < 0 || nx >= maze[0].length || ny < 0 || ny >= maze.length) {
+                continue;
+            }
+            if (maze[ny][nx] == '_' || maze[ny][nx] == 'x') {
+                continue;
+            }
+            nextAvailablePoints.add(new Point(nx, ny));
+        }
+        return nextAvailablePoints;
     }
 
     private static class Node {
--- Lab8/src/academy/pocu/comp3500/lab8/maze/Point.java
+++ Lab8/src/academy/pocu/comp3500/lab8/maze/Point.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class Point {
-    private static final int[] dx = new int[]{1, 0, -1, 0};
-    private static final int[] dy = new int[]{0, 1, 0, -1};
-
     private final int x;
     private final int y;
 
@@ -21,21 +18,5 @@ public final class Point {
 
     public int getY() {
         return this.y;
-    }
-
-    public List<Point> getNextAvailablePoints(final char[][] maze) {
-        List<Point> nextAvailablePoints = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            int nx = this.x + dx[i];
-            int ny = this.y + dy[i];
-            if (nx < 0 || nx >= maze[0].length || ny < 0 || ny >= maze.length) {
-                continue;
-            }
-            if (maze[ny][nx] == '_' || maze[ny][nx] == 'x') {
-                continue;
-            }
-            nextAvailablePoints.add(new Point(nx, ny));
-        }
-        return nextAvailablePoints;
     }
 }
