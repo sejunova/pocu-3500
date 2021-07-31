d29da1e9f10199e1d4ffdb7b94d83760e640a362
sejun <sejun@quryon.com>
2021-07-31 11:20:38 +0900

    [Ass4] 맨먼스 최적화

--- Assignment4/src/academy/pocu/comp3500/assignment4/Project.java
+++ Assignment4/src/academy/pocu/comp3500/assignment4/Project.java
@@ -4,32 +4,30 @@ import academy.pocu.comp3500.assignment4.project.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public final class Project {
-    private Map<String, Task> taskMap;
     private Task[] tasks;
 
     public Project(final Task[] tasks) {
         this.tasks = tasks;
-        taskMap = new HashMap<>();
-        for (Task t: tasks) {
-            taskMap.put(t.getTitle(), t);
-        }
     }
 
     public int findTotalManMonths(final String task) {
         int manMonths = 0;
         List<Task> tasksForMs = new ArrayList<>();
         Map<String, Boolean> visited = new HashMap<>();
-        for (Task t: tasks) {
-            visited.put(t.getTitle(), false);
+        Task mileStone = null;
+        for (Task t : tasks) {
+            if (t.getTitle().equals(task)) {
+                mileStone = t;
+                break;
+            }
         }
-        dfsRecursive(taskMap.get(task), visited, tasksForMs);
-        for (Task t: tasksForMs) {
+        assert mileStone != null;
+        dfsRecursive(mileStone, visited, tasksForMs);
+        for (Task t : tasksForMs) {
             manMonths += t.getEstimate();
         }
         return manMonths;
@@ -45,7 +43,7 @@ public final class Project {
 
     private static void dfsRecursive(Task task, Map<String, Boolean> visited, List<Task> stack) {
         for (Task child : task.getPredecessors()) {
-            if (!visited.get(child.getTitle())) {
+            if (!visited.containsKey(child.getTitle())) {
                 visited.put(child.getTitle(), true);
                 dfsRecursive(child, visited, stack);
             }
